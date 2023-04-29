f27674a47665ba3c29a526b998ada101e80b0ed1
mvr <matt@example.com>
2023-04-29 16:30:34 -0400

    sq

--- src/pynchon/abcs/meta.py
+++ src/pynchon/abcs/meta.py
@@ -1,8 +1,12 @@
 """ pynchon.abcs.meta
 """
+import collections
+
 from pynchon.util import typing, lme
 
 LOGGER = lme.get_logger(__name__)
+
+type_spec = collections.namedtuple('type_spec', 'name bases namespace')
 
 
 class Meta(type):
@@ -10,51 +14,55 @@ class Meta(type):
 
     NAMES = []
 
-    def __new__(mcls: type, name, bases, namespace):
+    def __new__(mcls: type, name: str, bases: typing.List, namespace: typing.Dict):
         """ """
-        mcls.register(name=name, bases=bases, namespace=namespace)
-        namespace = mcls.annotate(name=name, bases=bases, namespace=namespace)
+        tspec = type_spec(name=name, bases=bases, namespace=namespace)
+        mcls.register(tspec)
+        namespace = mcls.annotate(tspec)
         return super().__new__(mcls, name, bases, namespace)
 
-    @classmethod
-    def aggr(
-        mcls: type,
-        attr: str = '',
-        bases: typing.List = [],
-        namespace: typing.Dict = {},
+    @staticmethod
+    def aggregate_across_bases(
+        var: str = '',
+        tspec:type_spec=None,
     ):
-        """ """
-        class_props = namespace.get(attr, [])
-        for b in bases:
-            class_props += getattr(b, attr, [])
-        return class_props
+        """
+        aggregate_across_basesegates values at `var` across all bases
+        """
+        tracked = tspec.namespace.get(var, [])
+        for b in tspec.bases:
+            tracked += getattr(b, var, [])
+        return tracked
 
     @classmethod
     def register(
         mcls: type,
-        name: str = '',
-        bases: typing.List = [],
-        namespace: typing.Dict = {},
+        tspec:type_spec=None,
     ) -> None:
         """ """
+        name,bases,namespace=tspec.name, tspec.bases, tspec.namespace
         this_name = namespace.get('name', None)
         this_name and Meta.NAMES.append(this_name)
 
     @classmethod
     def annotate(
         mcls: type,
-        name: str = '',
-        bases: typing.List = [],
-        namespace: typing.Dict = {},
+        tspec:type_spec=None,
     ) -> typing.Dict:
         """ """
-        class_props = mcls.aggr('__class_properties__', bases, namespace)
+        name,bases,namespace=tspec.name, tspec.bases, tspec.namespace
+        class_props = mcls.aggregate_across_bases(
+            var='__class_properties__',
+            tspec=tspec,
+        )
         class_props += [
             k for k, v in namespace.items() if isinstance(v, typing.classproperty)
         ]
         class_props = list(set(class_props))
         namespace.update({'__class_properties__': class_props})
-        instance_methods = mcls.aggr('__methods__', bases, namespace)
+        instance_methods = mcls.aggregate_across_bases(
+            var='__methods__', tspec=tspec
+        )
         instance_methods += [
             k
             for k, v in namespace.items()
@@ -62,7 +70,9 @@ class Meta(type):
         ]
         instance_methods = list(set(instance_methods))
         namespace.update({'__methods__': instance_methods})
-        instance_properties = mcls.aggr('__properties__', bases, namespace)
+        instance_properties = mcls.aggregate_across_bases(
+            var='__properties__',  tspec=tspec,
+        )
         instance_properties += [
             k
             for k, v in namespace.items()
