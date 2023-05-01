b4616a30bae75595a616d8180cf8006296a77024
mvr <matt@example.com>
2023-04-30 22:03:30 -0400

    sq

--- src/pynchon/util/text/__main__.py
+++ src/pynchon/util/text/__main__.py
@@ -7,24 +7,19 @@ from pynchon.util import lme, typing
 LOGGER = lme.get_logger(__name__)
 entry = common.entry_for(__name__)
 
-registry = (
-    shimport.wrap('pynchon.util.text')
-    .filter_folder(include_main=True)
-    .prune(
-        name_is='entry',  # default
-    )
-    .map(
-        lambda ch: [
-            ch.name.replace('.__main__', '').split('.')[-1],
-            ch.namespace['entry'],
-        ]
-    )
-    .starmap(
-        lambda name, fxn: [
-            setattr(fxn, 'name', name),
-            click.group_copy(fxn, entry),
-        ]
-    )
+
+shimport.wrap('pynchon.util.text').filter_folder(include_main=True).prune(
+    name_is='entry',  # default
+).map(
+    lambda ch: [
+        ch.name.replace('.__main__', '').split('.')[-1],
+        ch.namespace['entry'],
+    ]
+).starmap(
+    lambda name, fxn: [
+        setattr(fxn, 'name', name),
+        click.group_copy(fxn, entry),
+    ]
 )
 
 if __name__ == '__main__':
