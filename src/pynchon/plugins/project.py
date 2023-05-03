96ccf328b9456a92d0aaad34324836310f3e3b58
mvr <matt@example.com>
2023-05-02 20:17:09 -0400

    sq

--- src/pynchon/plugins/project.py
+++ src/pynchon/plugins/project.py
@@ -55,11 +55,10 @@ class Project(models.Manager):
 
     name = 'project'
     priority = 2
-    defaults = dict()
     config_class = ProjectConfig
 
     @classmethod
-    def init_cli(kls):
+    def asdinit_cli(kls):
         """pynchon.bin.project"""
         parent = kls.click_group
 
@@ -70,52 +69,30 @@ class Project(models.Manager):
 
         LOGGER = lme.get_logger(__name__)
 
-        @common.kommand(
-            name="entrypoints",
-            parent=parent,
-            formatters=dict(markdown=constants.T_TOC_CLI),
-            options=[
-                options.file_setupcfg,
-                options.format,
-                options.stdout,
-                options.output,
-                options.header,
-            ],
-        )
-        def project_entrypoints(format, file, stdout, output, header) -> None:
-            """
-            Describe entrypoints for this project (parses setup.cfg)
-            """
-            config, plan = project.plan()
-            return {
-                **util.python.load_entrypoints(util.python.load_setupcfg(file=file)),
-                **dict(__main__=config.get("source", {}).get("__main__", [])),
-            }
-
-        @common.kommand(
-            name="version",
-            parent=parent,
-            formatters=dict(markdown=constants.T_VERSION_METADATA),
-            options=[
-                # FIXME: options.output_with_default('docs/VERSION.md'),
-                options.format_markdown,
-                options.output,
-                options.header,
-            ],
-        )
-        def project_version(format, output, header) -> None:
-            """
-            Describes version details for this package (and pynchon itself).
-            """
-            # from pynchon.api import python #, git
-            import pynchon
-            from pynchon.config import git, python
-
-            return dict(
-                pynchon_version=pynchon.__version__,
-                package_version=python.package.version,
-                git_hash=git.hash,
-            )
+        # @common.kommand(
+        #     name="version",
+        #     parent=parent,
+        #     formatters=dict(markdown=constants.T_VERSION_METADATA),
+        #     options=[
+        #         # FIXME: options.output_with_default('docs/VERSION.md'),
+        #         options.format_markdown,
+        #         options.output,
+        #         options.header,
+        #     ],
+        # )
+        # def project_version(format, output, header) -> None:
+        #     """
+        #     Describes version details for this package (and pynchon itself).
+        #     """
+        #     # from pynchon.api import python #, git
+        #     import pynchon
+        #     from pynchon.config import git, python
+        #
+        #     return dict(
+        #         pynchon_version=pynchon.__version__,
+        #         package_version=python.package.version,
+        #         git_hash=git.hash,
+        #     )
 
         @parent.command(
             name="config",
