f5274051ebfcde1a8fbb83f564571b3696f5b431
mvr <matt@example.com>
2023-05-05 15:07:32 -0400

    sq

--- src/pynchon/util/shfmt/__main__.py
+++ src/pynchon/util/shfmt/__main__.py
@@ -11,24 +11,9 @@ if __name__ == '__main__':
         line-continued \
         string \
     ' && echo -n1 '\
-    hello world' zonk \
+    hello world' \
     || bonk one \
-    || zoooom
+    ; zoooom
     """
-    print(f'parsing:\n\n{cmd}\n\n')
-    tmp = grammar.BashCommand().parseString(cmd)
-    print(f'parsed:\n\n')
-    pprint.pprint(list(tmp))
-    print()
-    print()
-    print(f'flattened:\n\n')
-    tmp2 = [
-        item
-        for sublist in [x for x in tmp if not isinstance(x, (str,))]
-        for item in sublist
-    ]
-    pprint.pprint(list(tmp2))
-    print()
-    print()
-    tmp = grammar.bash_fmt(tmp)
+    tmp = grammar.bash_fmt(cmd)
     print(f'formatted:\n\n{tmp}\n\n')
--- src/pynchon/util/shfmt/grammar.py
+++ src/pynchon/util/shfmt/grammar.py
@@ -27,7 +27,12 @@ QArg = QuotedString("\'", multiline=True) | QuotedString('\"', multiline=True)
 QArg.setParseAction(QString)
 
 Continuation = ('\\' + (LineEnd())).suppress()
-CommandJoiner = (Literal('&&') | Literal('||') | Literal(';')).setResultsName('joiner')
+CommandJoiner = Literal('&&')
+CommandJoiner|= Literal('||')
+CommandJoiner|= Literal(';') | pyparsing.LineStart()
+# CommandJoiner|=pyparsing.LineStart()
+CommandJoiner = CommandJoiner.setResultsName('joiner')
+
 Name = Word(alphanums)
 
 Arg = Name('argval') + Optional(Continuation)
@@ -35,57 +40,74 @@ Arg = Arg('argval')
 Vals = Group(ZeroOrMore(Arg | QArg('quoted_arg')))
 
 Option = Literal("-").suppress() + Name('short_option_name')
-Option = Group(Option+ Vals('vals'))
+Option = Group(Option + Vals('vals'))
 LongOption = Literal("--").suppress() + Name('long_option_name')
-LongOption = Group(LongOption+ Vals('vals'))
+LongOption = Group(LongOption + Vals('vals'))
 Options = ZeroOrMore(Option | LongOption)
 
 CommandName = Name('name')
 Command = Combine(CommandName)('cmd')
-Command+= Options('cmd_opts')
+Command+= Optional(Options('cmd_opts'))
+Command+= Optional(Vals('cmd_args'))
 
-JoinedCommand = ZeroOrMore(Group(CommandJoiner + Command)('cmd'))('tail')
-# JoinedCommand = Group(JoinedCommand)
+JoinedCommand = ZeroOrMore(Group(CommandJoiner + Command)('cmd'))
+#
+# BashCommand = Command('cmd').set_results_name('cmd')
+# BashCommand+= Optional(JoinedCommand)
 
-BashCommand = Command('cmd').set_results_name('cmd')
-BashCommand+= Optional(JoinedCommand)
-
-
-def fmt_token(token, level=0):
+BashCommand=JoinedCommand
+def fmt_token(token, indent=''):
     """ """
     result = []
-    if isinstance(token, (str,)):
+    if isinstance(token, (str,)) and token:
         raise Exception(f'expected token! got {token}')
-    else:
-        tdict = dict(token)
-        if 'vals' not in tdict:
-            vals = ''
-        else:
-            vals = []
-            vdict = dict(tdict['vals'])
-            for v in tdict['vals']:
-                if 'quoted_arg' in vdict:
-                    vals.append(f"'{v}'")
-                else:
-                    vals.append(f"{v}")
-            vals = ' '.join(vals)
-    if 'name' in tdict:
-        return token.name
-    if 'long_option_name' in tdict:
-        return f' --{token.long_option_name} {vals}'
-    elif 'short_option_name' in tdict:
-        pre = '-'
-        return f' -{token.short_option_name} {vals}'
-    elif 'cmd' in tdict:
+
+    tdict = dict(token)
+    vals=[]
+    if 'vals' in tdict:
+        vdict = dict(tdict['vals'])
+        for v in tdict['vals']:
+            if 'quoted_arg' in vdict:
+                vals.append(f"'{v}'")
+            else:
+                vals.append(f"{v}")
+    if 'argval' in tdict:
+        argval = token.argval[0]
+        if argval:
+            vals+=list(argval)
+    vals = ' '.join(vals)
+
+    if 'cmd' in tdict:
         cmd_opts = tdict.get('cmd_opts',[])
-        cmd_opts = [fmt_token(t) for t in cmd_opts]
+        cmd_opts = [fmt_token(t,indent='\n') for t in cmd_opts]
         cmd_opts = ' '.join(cmd_opts)
-        return f'{token.joiner} {token.cmd.name} {cmd_opts}'
+        cmd_args = tdict.get('cmd_args',[])
+        cmd_args = [t for t in cmd_args]
+        cmd_args = ' '.join(cmd_args)
+        return f'{token.joiner} {token.cmd.name} {cmd_opts} {cmd_args}'
+    elif 'long_option_name' in tdict:
+        return f'{indent}  --{token.long_option_name} {vals}'
+    elif 'short_option_name' in tdict:
+        return f'{indent}  -{token.short_option_name} {vals}'
     else:
-        raise Exception(f'dont know how to fmt {tdict}')
+        logger(f'dont know how to fmt {[token,tdict]}')
+        return ''
 
-
-def bash_fmt(parsed, level=0):
+def bash_fmt(text, indent=''):
     """ """
-    result = [ fmt_token(token, level=level) for token in parsed]
-    return '\n'.join(result)
+    text=text.lstrip()
+    print(f'parsing:\n\n{text}\n\n')
+    parsed = BashCommand().parseString(text)
+    print(f'parsed:\n\n')
+    import pprint
+    pprint.pprint(list(enumerate(list(parsed))))
+    # print() ; print() ; print(f'flattened:\n\n')
+    # tmp2 = [
+    #     item
+    #     for sublist in [x for x in tmp if not isinstance(x, (str,))]
+    #     for item in sublist
+    # ]
+    # pprint.pprint(list(tmp2)); print(); print()
+
+    result = [ fmt_token(token, indent=indent) for token in parsed]
+    return '\n'.join(result).lstrip()
