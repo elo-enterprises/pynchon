40fc67927395593df49135b6dd70c04e0d36d228
mvr <matt@example.com>
2023-05-06 16:13:07 -0400

    sq

--- smoke-test.sh
+++ smoke-test.sh
@@ -6,7 +6,8 @@ python -mpynchon.util.text loadf --help
 python -mpynchon.util.text.render --help
 python -mpynchon.util.text render --help
 pynchon raw | jq .config_files
-pynchon config
+pynchon cfg
 # python -mpynchon.util.text.render jinja README.md.j2 --output /dev/null
 pynchon jinja list
 pynchon jinja plan
+pynchon src plan
