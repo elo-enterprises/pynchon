6d2aa26001db9a6fcde76adb80fec1cb286ae2fb
mvr <matt@example.com>
2023-05-18 21:20:17 -0400

    sq

--- src/pynchon/plugins/gen.py
+++ src/pynchon/plugins/gen.py
@@ -14,19 +14,26 @@ class Generators(models.NameSpace):
     name = cli_name = "gen"
     priority = 1
     config_class = None
+    @typing.classproperty
+    def siblings_with_gen(kls):
+        result = {}
+        for name,obj in kls.siblings.items():
+            if obj.name=='core':
+                continue
+            elif 'gen' in dir(obj):
+                result[obj.name]=obj.gen
+        return result
+
 
     @classmethod
     def init_cli(kls) -> cli.click.Group:
         """ """
         result = super(kls,kls).init_cli()
-        for name,obj in kls.siblings.items():
-            if obj.name=='core':
-                continue
-            elif 'gen' in dir(obj):
-                LOGGER.critical(f'acquiring {obj.gen}')
-                kls.click_acquire(
-                    obj.gen,
-                    copy=True,
-                    name=obj.name,
-                    help=f'Alias for `{kls.click_entry.name} {obj.name} gen`')
+        for sibling, cmd in kls.siblings_with_gen.items():
+            LOGGER.critical(f'acquiring {cmd} from {sibling}')
+            kls.click_acquire(
+                cmd,
+                copy=True,
+                name=sibling.name,
+                help=f'Alias for `{kls.click_entry.name} {sibling.name} gen`')
         return result
